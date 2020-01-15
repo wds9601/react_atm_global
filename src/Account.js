4329c608836b61aef189ca424023f98a5bfbe943
William Downey-Schell <Will@Williams-MacBook-Air.local>
2020-01-15 09:36:50 -0800

    completed GA calculator

--- src/Account.js
+++ src/Account.js
@@ -9,20 +9,40 @@ export default class Account extends Component {
     }
 
     this.handleDepositClick = this.handleDepositClick.bind(this)
+    this.handleWithdrawClick = this.handleWithdrawClick.bind(this)
   }
 
   handleDepositClick(e) {
     e.preventDefault();
     if (isNaN(this.refs.amount.value)) {
       console.log("Not a number");
-    }
-    else {
+    } else if (-(this.refs.amount.value)) {
+      this.refs.amount.value = 0
+      console.log('Can\'t deposit negatives!')
+    } else {
       let amount = +this.refs.amount.value;
       let newBalance = this.state.balance + amount;
       this.setState({
         balance: newBalance
       })
       this.refs.amount.value = '';
+    }
+  }
+
+  handleWithdrawClick(e) {
+    e.preventDefault()
+    if (isNaN(this.refs.amount.value)) {
+      console.log('Not a number!')
+    } else if (-(this.refs.amount.value)) {
+      this.refs.amount.value = 0
+      console.log('Can\'t deposit negatives!')
+    } else {
+      let amount = +this.refs.amount.value
+      let newBalance = this.state.balance - amount
+      this.setState ({
+        balance: newBalance
+      })
+      this.refs.amount.value = ''
     }
   }
 
@@ -38,6 +58,7 @@ export default class Account extends Component {
         <div className={balanceClass}>${this.state.balance}</div>
         <input type="text" placeholder="enter an amount" ref="amount" />
         <input type="button" value="Deposit" onClick={this.handleDepositClick} />
+        <input type="button" value="Withdraw" onClick={this.handleWithdrawClick} />
       </div>
     )
   }
